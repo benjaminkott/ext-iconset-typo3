mediaessenz/ext-iconset-bootstrap

[TASK] Initialize

.php-cs-fixer.dist.php
@@ -1,0 +1,74 @@
+<?php
+
+/**
+ * This file represents the configuration for Code Sniffing PSR-2-related
+ * automatic checks of coding guidelines
+ * Install @fabpot's great php-cs-fixer tool via
+ *
+ *  $ composer global require friendsofphp/php-cs-fixer
+ *
+ * And then simply run
+ *
+ *  $ php-cs-fixer fix
+ *
+ * For more information read:
+ *  http://www.php-fig.org/psr/psr-2/
+ *  http://cs.sensiolabs.org
+ */
+
+if (PHP_SAPI !== 'cli') {
+    die('This script supports command line usage only. Please check your command.');
+}
+
+$header = <<<EOF
+This file is part of the package bk2k/iconset-bootstrap.
+
+For the full copyright and license information, please read the
+LICENSE file that was distributed with this source code.
+EOF;
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR2' => true,
+        'header_comment' => [
+            'header' => $header
+        ],
+        'general_phpdoc_annotation_remove' => [
+            'annotations' => [
+                'author'
+            ]
+        ],
+        'no_leading_import_slash' => true,
+        'no_trailing_comma_in_singleline_array' => true,
+        'no_singleline_whitespace_before_semicolons' => true,
+        'no_unused_imports' => true,
+        'concat_space' => ['spacing' => 'one'],
+        'no_whitespace_in_blank_line' => true,
+        'ordered_imports' => true,
+        'single_quote' => true,
+        'no_empty_statement' => true,
+        'no_extra_blank_lines' => true,
+        'phpdoc_no_package' => true,
+        'phpdoc_scalar' => true,
+        'no_blank_lines_after_phpdoc' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'whitespace_after_comma_in_array' => true,
+        'function_typehint_space' => true,
+        'single_line_comment_style' => true,
+        'no_alias_functions' => true,
+        'lowercase_cast' => true,
+        'no_leading_namespace_whitespace' => true,
+        'native_function_casing' => true,
+        'self_accessor' => true,
+        'no_short_bool_cast' => true,
+        'no_unneeded_control_parentheses' => true
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->exclude('.build')
+            ->exclude('config')
+            ->exclude('node_modules')
+            ->exclude('var')
+            ->in(__DIR__)
+    );

ext_emconf.php
@@ -1,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the package bk2k/iconset-bootstrap.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Iconset Bootstrap',
+    'description' => '',
+    'category' => 'templates',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0-12.4.99',
+            'bootstrap_package' => '12.0.0-13.99.99',
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'BK2K\\IconsetBootstrap\\' => 'Classes'
+        ],
+    ],
+    'state' => 'stable',
+    'author' => 'Benjamin Kott',
+    'author_email' => 'info@bk2k.info',
+    'author_company' => 'private',
+    'version' => '1.0.0',
+];

ext_localconf.php
@@ -1,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the package bk2k/iconset-bootstrap.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+defined('TYPO3') || die();
+
+use BK2K\IconsetBootstrap\Icons\IconProvider;
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/bootstrap-package/icons']['provider'][IconProvider::class]
+    = IconProvider::class;

benjaminkott/ext-iconset-typo3

.php-cs-fixer.dist.php
@@ -1,0 +1,74 @@
+<?php
+
+/**
+ * This file represents the configuration for Code Sniffing PSR-2-related
+ * automatic checks of coding guidelines
+ * Install @fabpot's great php-cs-fixer tool via
+ *
+ *  $ composer global require friendsofphp/php-cs-fixer
+ *
+ * And then simply run
+ *
+ *  $ php-cs-fixer fix
+ *
+ * For more information read:
+ *  http://www.php-fig.org/psr/psr-2/
+ *  http://cs.sensiolabs.org
+ */
+
+if (PHP_SAPI !== 'cli') {
+    die('This script supports command line usage only. Please check your command.');
+}
+
+$header = <<<EOF
+This file is part of the package bk2k/iconset-typo3.
+
+For the full copyright and license information, please read the
+LICENSE file that was distributed with this source code.
+EOF;
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR2' => true,
+        'header_comment' => [
+            'header' => $header
+        ],
+        'general_phpdoc_annotation_remove' => [
+            'annotations' => [
+                'author'
+            ]
+        ],
+        'no_leading_import_slash' => true,
+        'no_trailing_comma_in_singleline_array' => true,
+        'no_singleline_whitespace_before_semicolons' => true,
+        'no_unused_imports' => true,
+        'concat_space' => ['spacing' => 'one'],
+        'no_whitespace_in_blank_line' => true,
+        'ordered_imports' => true,
+        'single_quote' => true,
+        'no_empty_statement' => true,
+        'no_extra_blank_lines' => true,
+        'phpdoc_no_package' => true,
+        'phpdoc_scalar' => true,
+        'no_blank_lines_after_phpdoc' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'whitespace_after_comma_in_array' => true,
+        'function_typehint_space' => true,
+        'single_line_comment_style' => true,
+        'no_alias_functions' => true,
+        'lowercase_cast' => true,
+        'no_leading_namespace_whitespace' => true,
+        'native_function_casing' => true,
+        'self_accessor' => true,
+        'no_short_bool_cast' => true,
+        'no_unneeded_control_parentheses' => true
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->exclude('.build')
+            ->exclude('config')
+            ->exclude('node_modules')
+            ->exclude('var')
+            ->in(__DIR__)
+    );

ext_emconf.php
@@ -1,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the package bk2k/iconset-typo3.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Iconset TYPO3',
+    'description' => 'TYPO3 Iconset for the Boostrap Package',
+    'category' => 'templates',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0-12.4.99',
+            'bootstrap_package' => '12.0.0-13.99.99',
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'BK2K\\IconsetTypo3\\' => 'Classes'
+        ],
+    ],
+    'state' => 'stable',
+    'author' => 'Benjamin Kott',
+    'author_email' => 'info@bk2k.info',
+    'author_company' => 'private',
+    'version' => '1.0.0',
+];

ext_localconf.php
@@ -1,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the package bk2k/iconset-typo3.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+defined('TYPO3') || die();
+
+use BK2K\IconsetTypo3\Icons\IconProvider;
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/bootstrap-package/icons']['provider'][IconProvider::class]
+    = IconProvider::class;